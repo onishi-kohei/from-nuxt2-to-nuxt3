ca56c70d4e24869af9cf6b4b16620b9172641c3e
onishi-kohei <kohei.o2222@gmail.com>
2022-11-09 09:02:30 +0900

    metaの変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,20 +5,22 @@ export default defineNuxtConfig({
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'app',
-    htmlAttrs: {
-      lang: 'en'
+  app:{
+    head: {
+      title: 'app',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
